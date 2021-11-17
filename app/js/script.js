c4a5690840084c1ad908dcc7263140a259fa2902
nurgeldi atamyradov <67277491+nurgeldiatamyradov@users.noreply.github.com>
2021-11-17 09:38:27 +0300

    mobile menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,30 @@
-console.log("helloooo")
+const btnHamburger = document.querySelector('#btnHamburger');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+const fadeElements = document.querySelector('.has-fade');
+
+
+btnHamburger.addEventListener('click', function(){
+    console.log('click hamburger');
+
+    if(header.classList.contains('open')) { // Close Hamburger Menu
+        header.classList.remove('open');
+        fadeElements.forEach(function(element){
+            element.classList.add('fade-out');
+            element.classList.remove('fade-in');
+        })
+        
+
+
+    }
+    else { // Open Hamburger Menu
+        header.classList.add('open');
+        fadeElements.forEach(function(element){
+            element.classList.add('fade-in');
+            element.classList.remove('fade-out');
+        })
+        
+
+
+    }
+});
