85b2aaa3d9a480ff3e8eee50ebb436bf8cf2b596
nurgeldi atamyradov <67277491+nurgeldiatamyradov@users.noreply.github.com>
2021-11-18 18:41:46 +0300

    features done

--- app/js/script.js
+++ app/js/script.js
@@ -1,7 +1,8 @@
 const btnHamburger = document.querySelector('#btnHamburger');
+const body = document.querySelector('body');
 const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
-const fadeElements = document.querySelector('.has-fade');
+const fadeElements = document.querySelectorAll('.has-fade');
 
 
 btnHamburger.addEventListener('click', function(){
@@ -9,6 +10,7 @@ btnHamburger.addEventListener('click', function(){
 
     if(header.classList.contains('open')) { // Close Hamburger Menu
         header.classList.remove('open');
+        body.classList.remove('noscroll');
         fadeElements.forEach(function(element){
             element.classList.add('fade-out');
             element.classList.remove('fade-in');
@@ -18,6 +20,7 @@ btnHamburger.addEventListener('click', function(){
 
     }
     else { // Open Hamburger Menu
+        body.classList.add('noscroll');
         header.classList.add('open');
         fadeElements.forEach(function(element){
             element.classList.add('fade-in');
