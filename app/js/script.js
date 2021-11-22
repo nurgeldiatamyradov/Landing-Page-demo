954d70fa95821ef17aa08463e3aea9fd73368446
nurgeldi atamyradov <67277491+nurgeldiatamyradov@users.noreply.github.com>
2021-11-22 18:17:22 +0300

    Hamburger bug is fixed

--- app/js/script.js
+++ app/js/script.js
@@ -2,32 +2,27 @@ const btnHamburger = document.querySelector('#btnHamburger');
 const body = document.querySelector('body');
 const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
-const fadeElements = document.querySelectorAll('.has-fade');
-
+const fadeElems = document.querySelectorAll('.has-fade');
 
 btnHamburger.addEventListener('click', function(){
-    console.log('click hamburger');
+  console.log('click hamburger');
 
-    if(header.classList.contains('open')) { // Close Hamburger Menu
-        header.classList.remove('open');
-        body.classList.remove('noscroll');
-        fadeElements.forEach(function(element){
-            element.classList.add('fade-out');
-            element.classList.remove('fade-in');
-        })
-        
+  if(header.classList.contains('open')){ // Close Hamburger Menu
+    body.classList.remove('noscroll');
+    header.classList.remove('open');    
+    fadeElems.forEach(function(element){
+      element.classList.remove('fade-in');
+      element.classList.add('fade-out');
+    });
+    
+  }
+  else { // Open Hamburger Menu
+    body.classList.add('noscroll');
+    header.classList.add('open');
+    fadeElems.forEach(function(element){
+      element.classList.remove('fade-out');
+      element.classList.add('fade-in');
+    });
 
-
-    }
-    else { // Open Hamburger Menu
-        body.classList.add('noscroll');
-        header.classList.add('open');
-        fadeElements.forEach(function(element){
-            element.classList.add('fade-in');
-            element.classList.remove('fade-out');
-        })
-        
-
-
-    }
+  }  
 });
